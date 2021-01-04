1629d13471776747475d945630f545c7c59ead5e
David <ddropping@gmail.com>
2021-01-04 13:35:58 -0800

    Initial commit

---
+++ Swiper.py
@@ -0,0 +1,14 @@
+# Sign into amazon account
+
+# Find the product (within price range)
+
+# Wait until the product is available
+
+# Add to cart
+
+# Add payment methon (if applicable)
+
+# checkout
+
+from selenium import webdriver
+PATH = "C:\Program Files (x86)\chromedriver.exe"
